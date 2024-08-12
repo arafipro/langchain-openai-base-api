a567fff1e91407137f648ac8efe0a73fab5ee304
Hiroki Chiyoda <contact@arafipro.com>
2024-08-12 14:56:37 +0900

    feat: install-hono2

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
+import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
+import { z } from "zod";
 
 const app = new Hono();
 
-app.post("/", async (c) => {
-  const body = await c.req.json();
+const schema = z.object({
+  prompt: z.string(),
+});
+
+app.post("/", zValidator("json", schema), async (c) => {
+  const body = await c.req.valid("json");
   return c.json(body);
 });
 
