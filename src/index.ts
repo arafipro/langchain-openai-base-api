1eab48e5c8981e930a4173beca8a548d005d03c7
Hiroki Chiyoda <contact@arafipro.com>
2024-08-12 23:56:11 +0900

    feat: instantiate_model

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,21 @@
 import { zValidator } from "@hono/zod-validator";
+import { ChatOpenAI } from "@langchain/openai";
 import { Hono } from "hono";
 import { z } from "zod";
 
-const app = new Hono();
+type Bindings = {
+  OPENAI_API_KEY: string;
+};
+
+const app = new Hono<{ Bindings: Bindings }>();
 
 const schema = z.object({
   prompt: z.string(),
 });
 
 app.post("/", zValidator("json", schema), async (c) => {
+  const apiKey = c.env.OPENAI_API_KEY;
+  const model = new ChatOpenAI({ apiKey: apiKey, model: "gpt-4o-mini" });
   const body = await c.req.valid("json");
   return c.json(body);
 });
