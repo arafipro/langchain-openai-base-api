4247366d1b178bd1719450fd09cc6d66802bc792
Hiroki Chiyoda <contact@arafipro.com>
2024-08-12 13:37:44 +0900

    feat: install hono

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
-export default {
-  async fetch(request, env, ctx): Promise<Response> {
-    return new Response("Hello World!");
-  },
-} satisfies ExportedHandler<Env>;
+import { Hono } from "hono";
+
+const app = new Hono();
+
+app.post("/", async (c) => {
+  const body = await c.req.json();
+  return c.json(body);
+});
+
+export default app;
