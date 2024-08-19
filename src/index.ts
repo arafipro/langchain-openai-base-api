23f444c6c48292dedc621336094b319a79c51ceb
Hiroki Chiyoda <contact@arafipro.com>
2024-08-19 10:25:07 +0900

    feat: chain-connect

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { zValidator } from "@hono/zod-validator";
-import { HumanMessage, SystemMessage } from "@langchain/core/messages";
+import { StringOutputParser } from "@langchain/core/output_parsers";
 import { ChatPromptTemplate } from "@langchain/core/prompts";
+import { Runnable } from "@langchain/core/runnables";
 import { ChatOpenAI } from "@langchain/openai";
 import { Hono } from "hono";
 import { z } from "zod";
@@ -17,21 +18,18 @@ const schema = z.object({
 
 app.post("/", zValidator("json", schema), async (c) => {
   const apiKey = c.env.OPENAI_API_KEY;
-  const model = new ChatOpenAI({ apiKey: apiKey, model: "gpt-4o-mini" });
-  const message = [
-    new SystemMessage("次の文章を英語から日本語に翻訳してください。"),
-    new HumanMessage("hi!"),
-  ];
+  const model = new ChatOpenAI({
+    apiKey: apiKey,
+    model: "gpt-4o-mini",
+  }) as unknown as Runnable;
   const systemTemplate = "次の文章を英語から{language}に翻訳してください。";
   const promptTemplate = ChatPromptTemplate.fromMessages([
     ["system", systemTemplate],
     ["user", "{text}"],
   ]);
-  const res = await promptTemplate.invoke({ language: "日本語", text: "hi" });
-  // const parser = new StringOutputParser();
-  // const chain = model.pipe(parser);
-  // const res = await chain.invoke(message);
-  // const body = await c.req.valid("json");
+  const parser = new StringOutputParser();
+  const chain = promptTemplate.pipe(model).pipe(parser);
+  const res = await chain.invoke({ language: "日本語", text: "hi" });
   return c.json(res);
 });
 
